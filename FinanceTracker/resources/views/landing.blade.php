5d0c4e7d4fdd4262fd57d15e4320c978edf9fa97
Ryan Wennekes <ryanwennekes@gmail.com>
2020-04-02 21:46:40 +0200

    Landing page styling

--- FinanceTracker/resources/views/landing.blade.php
+++ FinanceTracker/resources/views/landing.blade.php
@@ -4,10 +4,15 @@
     </head>
     <body>
         <h1>{{config('app.name')}}</h1>
-        <form>
-            <input type="email" placeholder="E-mail"/>
-            <input type="password" placeholder="Password"/>
-            <input type="submit" value="Login" />
-        </form>
+        <p>
+            <span class="stress">Become debt-free,</span> <span class="stress">FOR free!</span>
+        </p>
+        <div class="options">
+            @include('components/login-key')
+            @include('components/signup-pen')
+        </div>
+
+    <script> </script>
+    @stack('script')
     </body>
 </html>
